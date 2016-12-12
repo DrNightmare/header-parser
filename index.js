401b9023a41f5bc9334d842f36ce8fe0ca3eacb0
Arvind <arvind687@gmail.com>
2016-12-12 23:29:22 +0530

    Modified port to support Heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res) {
 	res.send({"ipaddress":ip, "language":lang, "software": software});
 });
 
-app.listen(3000, function(err, data){
+app.listen(process.env.PORT || 5000, function(err, data){
 	if (err) console.log(err);
-	console.log("Listening on port 3000");
+	console.log("Listening on port 5000");
 });
